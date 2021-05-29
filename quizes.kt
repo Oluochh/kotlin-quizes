a8bf0182b7ead9d518aad93fddb66fea735647ad
Oluochh <79199252+Oluochh@users.noreply.github.com>
2021-05-29 09:50:42 +0300

    Add files via upload

---
+++ quizes.kt
@@ -0,0 +1,28 @@
+fun main() {
+    var revBook=books.reversed()
+    println(revBook)
+
+
+
+
+}
+data class Book(var Title:String,var Author:String,var Pages:Int)
+var books= listOf(
+    Book("Home body","Rupi kaur",218),
+    Book("Born a crime","Trevor Noah",288),
+    Book("Pretty this way","Sia mia",320),
+
+    )
+fun indices() {
+    for (x in 2)
+        if (x != 3000)
+}
+
+
+data class currentAccount(var accountNumber:Int,var accountName:String,var balance:Double){
+    var
+}
+
+
+
+
